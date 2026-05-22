9077fb73483f99f35032743153288e5fe0c6a60e
PawanKumar1981 <pawanautomationtesting@gmail.com>
2026-05-22 20:07:54 +0530

    Add JavaScript practice files and update First_JS_Program

--- First_JS_Program.js
+++ First_JS_Program.js
@@ -1,2 +1,10 @@
 
 console.log("Hello, World!");
+
+let num=5;
+
+if(num>=5){
+    console.log("num is greater than 5");
+}else{
+    console.log("num is not greater than 5");
+}
---
+++ My_Java_Script_Practice/Triangle_Classification_Program.js
@@ -0,0 +1,26 @@
+
+// Write a program that classifies a triangle based on its side lengths. Given three input values representing the lengths of the sides, determine if the triangle is equilateral (all sides are equal), 
+// isosceles (exactly two sides are equal), or scalene (no sides are equal).
+//  Use an if-else statement to classify the triangle.
+
+
+let side_A= 10;
+let side_B=5;
+let side_C=2;
+
+if(side_A === side_B && side_B=== side_C && side_C === side_A){
+
+    console.log("equilateral");
+
+}else if(side_A === side_B || side_B=== side_C || side_C === side_A){
+
+    console.log("isosceles");
+}else{
+
+    console.log("scalene");
+}
+
+
+
+
+
