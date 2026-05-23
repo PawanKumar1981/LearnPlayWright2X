d70c7d312b0b7d4f020a7b42cd846b9e09c5c254
PawanKumar1981 <pawanautomationtesting@gmail.com>
2026-05-23 12:57:26 +0530

    Add Grade_calculator program

---
+++ My_Java_Script_Practice/Grade_calculator.js
@@ -0,0 +1,12 @@
+let marks = 45;
+
+if (marks >= 80) {
+    console.log("Grade-A");
+} else if (marks >= 70) {
+    console.log("Garde-B");
+} else if (marks >= 60) {
+    console.log("Grade-C");
+} else {
+    console.log("fail")
+}
+
