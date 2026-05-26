a52dad4d969d2713939110ebbe64accb6c752c8c
PawanKumar1981 <pawanautomationtesting@gmail.com>
2026-05-26 20:37:36 +0530

    Add splice remove elements and slice copy array examples

---
+++ My_Java_Script_Practice/Splice_Remove_Elements.js
@@ -0,0 +1,4 @@
+let arr = [10, 20, 30, 40, 50];
+let removed = arr.splice(1, 2);
+console.log(removed);
+console.log(arr);
---
+++ My_Java_Script_Practice/slice_copy_array.js
@@ -0,0 +1,4 @@
+let arr = [10, 20, 30, 40, 50];
+let copyarr = arr.slice(1, 3);
+console.log(copyarr);
+console.log(arr);
