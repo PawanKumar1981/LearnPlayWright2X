9ff47975cc9565a57cc617c8034033c385a1ac22
PawanKumar1981 <pawanautomationtesting@gmail.com>
2026-06-21 12:00:58 +0530

    Add chapter 03 identifier and literals

---
+++ My_Java_Script_Practice/chapter_03_Identifier_Literals/06_Identifier_Rules.js
@@ -0,0 +1,14 @@
+var $ = 10;
+var _a = 12;
+var p = 10;
+
+var ab123 = 34;
+
+// var 123 = 123;
+
+var Name = "pavab";
+var name = "kumar";
+//var pramod dutta = "hello";
+var pavan_dutta = "hello";
+var pavan$dutta = "hello";
+var pavan1232 = "hello";
